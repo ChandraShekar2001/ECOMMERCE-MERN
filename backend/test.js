c9cbe72a7e8d24ea387188bfded9da0cea5e71cd
ChandraShekar2001 <pvchandu07@gmail.com>
2023-04-24 19:27:50 +0530

    added login testing

---
+++ backend/test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const login = require('../frontend/src/Components/Auth/login_test');
+
+describe('Login', function() {
+  describe('#authenticate()', function() {
+    it('should return true if the username and password are valid', function() {
+      assert.equal(login.authenticate('venkatachandrashekar.pviiits.in', 'password'), true);
+    });
+
+    it('should return false if the username is invalid', function() {
+      assert.equal(login.authenticate('invaliduser', 'password123'), false);
+    });
+
+    it('should return false if the password is invalid', function() {
+      assert.equal(login.authenticate('venkatachandrashekar.pv@iiits.in', ''), false);
+    });
+  });
+});
